4f12cc4f17e1a5a6f5846ffd5079e521b35fdc80
lOldSportl <mconor393@gmail.com>
2025-03-07 16:49:46 +0300

    added a check for the occurrence of the entered word in the dictionary of the game words

--- GameClass.java
+++ GameClass.java
@@ -9,36 +9,48 @@ import java.util.Scanner;
 
 public class GameClass {
 
-  static void start() {
-    Scanner in = new Scanner(System.in);
-    String correctWord = randomWord(); // загаданное слово
-    char[] correctArray = correctWord.toCharArray();
-    char[] wrongArray = new char[26]; // массив неправильных букв
-    char[] hiddenWord = new char[]{'*','*','*','*','*'}; // слово, которое отображается
-    System.out.println(correctWord);
-    String word = ""; // введеное слово
-    int attempt = 1;
-    do {
-    System.out.print(java.util.Arrays.toString(hiddenWord) + "\nEnter your word: ");
-    word = in.nextLine();
-    char[] wordArray = word.toCharArray();
-    if (checkWord(wordArray, correctArray, hiddenWord, wrongArray)) {
-      System.out.println(
-          "Congrats ! you succsessfully guessed the word");
-      break;
-    }
-    else 
-    {
-      System.out.println("Incorrect ! here is what you have guessed: " + java.util.Arrays.toString(hiddenWord) + "\nAttempt: " + attempt + 
-      "\nAttempts left: " + (6 - attempt) + "\nThese letters are wrong: " + java.util.Arrays.toString(wrongArray));
-      attempt++;
-    }
-  } while(attempt <= 6);
-    System.out.println("Game over !" + " Your stats:\n" + "The word was: " + correctWord + "\nAttempts taken: " + (attempt));
+  private static final Scanner sc = new Scanner(System.in);
 
+  static void start() {
+    String correctWord = randomWord(); // загаданное слово
+    // char[] correctArray = correctWord.toCharArray();
+    char[] hiddenWord = new char[] {'*', '*', '*', '*', '*'}; // слово, которое отображается
+    // System.out.println(correctWord); // вывод загаданного слова(для отладки)
+    String word = ""; // введеное слово
+    int attempt = 1; // попытки
+    do {
+      System.out.print(java.util.Arrays.toString(hiddenWord) + "\nEnter your word: ");
+      word = sc.nextLine();
+      System.out.println("Word written");
+      if(checkWordInList(word)){
+        System.out.println("Word exists in list");
+        // char[] wordArray = word.toCharArray();
+        if (checkWord(word, correctWord, hiddenWord)) {
+          System.out.println("Congrats ! you succsessfully guessed the word");
+          break;
+        } 
+      }
+      else {
+        System.out.println(
+            "Incorrect ! here is what you have guessed: "
+                + java.util.Arrays.toString(hiddenWord)
+                + "\nAttempt: "
+                + attempt
+                + "\nAttempts left: "
+                + (6 - attempt));
+        attempt++;
+      }
+    } while (attempt < 7);
+    System.out.println(
+        "Game over !"
+            + " Your stats:\n"
+            + "The word was: "
+            + correctWord
+            + "\nAttempts taken: "
+            + (attempt));   
   }
 
-  static String randomWord() {
+  static String randomWord() { // генерация случайного индекса из общего количества индексов в файле со словами
     Random rnd = new Random();
     long correctWordIndex = 1;
     try {
@@ -50,7 +62,7 @@ public class GameClass {
     return correctWord;
   }
 
-  public static long countWords() throws IOException {
+  public static long countWords() throws IOException { // подсчет общего числа слов
     long result;
     try (FileReader input = new FileReader("words.txt");
         LineNumberReader count = new LineNumberReader(input); ) {
@@ -62,17 +74,18 @@ public class GameClass {
     return result;
   }
 
-  public static String getWord(long correctWordIndex) {
+  public static String getWord(long correctWordIndex) { // получение  нужного слова из файла
     long currentWordIndex = 0;
     String line = "";
     try {
       File file = new File("words.txt");
       FileReader fr = new FileReader(file);
-      BufferedReader reader = new BufferedReader(fr);
-      line = reader.readLine();
-      while (currentWordIndex != correctWordIndex) {
+      try (BufferedReader reader = new BufferedReader(fr)) {
         line = reader.readLine();
-        currentWordIndex++;
+        while (currentWordIndex != correctWordIndex) {
+          line = reader.readLine();
+          currentWordIndex++;
+        }
       }
     } catch (FileNotFoundException e) {
       e.printStackTrace();
@@ -82,29 +95,36 @@ public class GameClass {
     return line;
   }
 
-  public static boolean checkWord(char[] wordArray, char[] correctArray, char[] hiddenWord, char[] wrongArray) {
-    //System.out.println(new String(wordArray) + "\n" + new String(correctArray));
-    if (new String(wordArray).equals(new String(correctArray))) {
+  public static boolean checkWord(String userWord, String targetWord, char[] hiddenLetter) {  // проверка слова
+
+    if (userWord.equals(targetWord)) {
       return true;
-    } 
-    else {
-      for (int i = 0; i <= wordArray.length - 1; i++) {
-        for (int j = 0; j <= wordArray.length - 1; j++) 
-          if (wordArray[i] == correctArray[j]) {
-            if(i == j) {
-              hiddenWord[i] = Character.toUpperCase(wordArray[i]);
-            }
-            else {
-              hiddenWord[i] = wordArray[i];
-            }
-          }
-          else if (wordArray[i] != correctArray[j]) {
-            wrongArray[i] = wordArray[j];
-          }
-
+    } else {
+      char[] userLetters = userWord.toCharArray();
+      char[] targetLetters = targetWord.toCharArray(); 
+      for (int i = 0; i < userLetters.length; i++){
+        if (Character.compare(userLetters[i], targetLetters[i]) == 0){
+            hiddenLetter[i] = Character.toUpperCase(userLetters[i]);
+        }
+        else if (new String(targetLetters).contains(String.valueOf(userLetters[i]))){
+          hiddenLetter[i] = userLetters[i];
+        }
       }
-      return false;
+    return false;
     }
+  }
 
+  public static boolean checkWordInList(String word) {
+    try (BufferedReader br = new BufferedReader(new FileReader("words.txt"))){
+      while (br.readLine() != null){
+        if (br.readLine().equals(word)){
+          return true;
+        }
+      }
+    }
+    catch (IOException e) {
+      System.out.println("Ошибка при чтении файла"); 
+    }
+    return false;
   }
 }
